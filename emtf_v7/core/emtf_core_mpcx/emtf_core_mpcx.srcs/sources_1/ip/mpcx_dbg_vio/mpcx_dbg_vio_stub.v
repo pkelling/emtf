d9386f47776fbd501618ecce37032f88c4c686a0
Alexander Madorsky <16050565+madorskya@users.noreply.github.com>
2020-10-02 13:02:28 -0400

    making sure all files added

---
+++ emtf_v7/core/emtf_core_mpcx/emtf_core_mpcx.srcs/sources_1/ip/mpcx_dbg_vio/mpcx_dbg_vio_stub.v
@@ -0,0 +1,27 @@
+// Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2019.2.1 (lin64) Build 2729669 Thu Dec  5 04:48:12 MST 2019
+// Date        : Wed Aug 12 11:50:43 2020
+// Host        : endcap-tf1.phys.ufl.edu running 64-bit CentOS Linux release 7.8.2003 (Core)
+// Command     : write_verilog -force -mode synth_stub
+//               /home/madorsky/github/emtf/emtf_v7/core/emtf_core_mpcx/emtf_core_mpcx.srcs/sources_1/ip/mpcx_dbg_vio/mpcx_dbg_vio_stub.v
+// Design      : mpcx_dbg_vio
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7vx690tffg1927-2
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+(* X_CORE_INFO = "vio,Vivado 2019.2.1" *)
+module mpcx_dbg_vio(clk, probe_out0, probe_out1, probe_out2, 
+  probe_out3, probe_out4, probe_out5)
+/* synthesis syn_black_box black_box_pad_pin="clk,probe_out0[2:0],probe_out1[3:0],probe_out2[2:0],probe_out3[3:0],probe_out4[2:0],probe_out5[3:0]" */;
+  input clk;
+  output [2:0]probe_out0;
+  output [3:0]probe_out1;
+  output [2:0]probe_out2;
+  output [3:0]probe_out3;
+  output [2:0]probe_out4;
+  output [3:0]probe_out5;
+endmodule
---
+++ emtf_v7/core/emtf_core_mpcx/emtf_core_mpcx.srcs/sources_1/ip/mpcx_dbg_vio/mpcx_dbg_vio_stub.vhdl
@@ -0,0 +1,36 @@
+-- Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2019.2.1 (lin64) Build 2729669 Thu Dec  5 04:48:12 MST 2019
+-- Date        : Wed Aug 12 11:50:43 2020
+-- Host        : endcap-tf1.phys.ufl.edu running 64-bit CentOS Linux release 7.8.2003 (Core)
+-- Command     : write_vhdl -force -mode synth_stub
+--               /home/madorsky/github/emtf/emtf_v7/core/emtf_core_mpcx/emtf_core_mpcx.srcs/sources_1/ip/mpcx_dbg_vio/mpcx_dbg_vio_stub.vhdl
+-- Design      : mpcx_dbg_vio
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7vx690tffg1927-2
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity mpcx_dbg_vio is
+  Port ( 
+    clk : in STD_LOGIC;
+    probe_out0 : out STD_LOGIC_VECTOR ( 2 downto 0 );
+    probe_out1 : out STD_LOGIC_VECTOR ( 3 downto 0 );
+    probe_out2 : out STD_LOGIC_VECTOR ( 2 downto 0 );
+    probe_out3 : out STD_LOGIC_VECTOR ( 3 downto 0 );
+    probe_out4 : out STD_LOGIC_VECTOR ( 2 downto 0 );
+    probe_out5 : out STD_LOGIC_VECTOR ( 3 downto 0 )
+  );
+
+end mpcx_dbg_vio;
+
+architecture stub of mpcx_dbg_vio is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "clk,probe_out0[2:0],probe_out1[3:0],probe_out2[2:0],probe_out3[3:0],probe_out4[2:0],probe_out5[3:0]";
+attribute X_CORE_INFO : string;
+attribute X_CORE_INFO of stub : architecture is "vio,Vivado 2019.2.1";
+begin
+end;
