d214693bd449a0a7227c95dd6042babe9da7d29f
Alexander Madorsky <16050565+madorskya@users.noreply.github.com>
2021-06-30 18:31:28 -0400

    adding LUT sources

---
+++ emtf_v7/core/emtf_data/ge11_lut/merge_each_sector.sh
@@ -0,0 +1,13 @@
+cat th_mem_endcap_1_sect_1_ch* > th_mem_endcap_1_sect_1.lut
+cat th_mem_endcap_1_sect_2_ch* > th_mem_endcap_1_sect_2.lut
+cat th_mem_endcap_1_sect_3_ch* > th_mem_endcap_1_sect_3.lut
+cat th_mem_endcap_1_sect_4_ch* > th_mem_endcap_1_sect_4.lut
+cat th_mem_endcap_1_sect_5_ch* > th_mem_endcap_1_sect_5.lut
+cat th_mem_endcap_1_sect_6_ch* > th_mem_endcap_1_sect_6.lut
+cat th_mem_endcap_2_sect_1_ch* > th_mem_endcap_2_sect_1.lut
+cat th_mem_endcap_2_sect_2_ch* > th_mem_endcap_2_sect_2.lut
+cat th_mem_endcap_2_sect_3_ch* > th_mem_endcap_2_sect_3.lut
+cat th_mem_endcap_2_sect_4_ch* > th_mem_endcap_2_sect_4.lut
+cat th_mem_endcap_2_sect_5_ch* > th_mem_endcap_2_sect_5.lut
+cat th_mem_endcap_2_sect_6_ch* > th_mem_endcap_2_sect_6.lut
+
